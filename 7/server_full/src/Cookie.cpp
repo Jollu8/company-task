0e46535421bd6acd84f04b2851c5dd9fe8194502
Jollu8 <zholu8@gmail.com>
2023-11-28 10:27:09 +0300

    !

--- 7/server_full/src/Cookie.cpp
+++ 7/server_full/src/Cookie.cpp
@@ -15,7 +15,7 @@ ParseResult<std::string> parseName(char const* str) {
     return parseUntil(str, [](char ch) { return std::isspace(ch) || ch == '='; });
 }
 
-ParseResult<std::string > ParseValue(char const * str) {
+ParseResult<std::string > parseValue(char const * str) {
     return parseUntil(str, [](char ch) {return ch == ';' || ch == '=';});
 }
 
@@ -30,6 +30,20 @@ ParseResult<std::string > parseSeparator(char const * str) {
     }
 }
 
-//Cookie parseCookie
+Cookie parseCookie(char const * str) {
+    auto name = parseName(str);
+    auto ch = parseSeparator(name.ch).ch;
+    auto value = parseValue(ch);
+    ch = parseSeparator(value.ch).ch;
 
+    auto cookie = Cookie();
+    cookie.nameIs(name.value);
+    cookie.valueIs(value.value);
+
+
+    while(*ch) {
+        auto flag = parseValue(ch);
+//        if()
+    }
+}
 }  // namespace http
